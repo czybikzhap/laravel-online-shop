84bf5b53cd8c0f2318551f7fdafa713efdf7fcfb
czybikzhap <czybikzhap@gmail.com>
2025-05-07 15:16:28 +0700

    добавление rabbit и ее реализация в регистрации

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Тестовое письмо</title>
+</head>
+<body>
+<h1>Привет!</h1>
+<p>Тестовое письмо от Laravel.</p>
+<p>Имя: {{ $data['name'] }}</p>
+</body>
+</html>
